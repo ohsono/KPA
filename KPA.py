a5140ed0d55abeacbc24096c555837f01571f759
Hochan Son <ohsono.email@gmail.com>
2020-10-12 18:10:06 -0700

    update kpa functions readme and requirements

--- KPA.py
+++ KPA.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
 import os
 import sys
 import json
 import logging
-from KafkaCLI.client import _Admin, Config
+# from confluent_kafka.admin import AdminClient, NewTopic
+from KafkaCLI.client import KafkaAdminCli, Config, Error
 
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+FORMAT = '%(asctime)-15s %(name)-5s %(levelname)-8s %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-out_hdlr = logging.StreamHandler(sys.stdout)
-logger.addHandler(out_hdlr)
+#out_hdlr = logging.StreamHandler(sys.stdout)
+#logger.addHandler(out_hdlr)
 
 __version__='1.0.0'
+#default_repl_factor=3
+#default_client_id='test'
 
 def command_line():
     '''
@@ -23,19 +25,30 @@ def command_line():
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter
                                         ,description='Kafka CLi tools (requirement python 3.x)')
-    parser.add_argument('-v', action='version',version='%(KPA) ver {}'.format(__version__), 
+    parser.add_argument('-v', action='version',version='%(aws-msk-cli)s ver {}'.format(__version__), 
             help='display app version')
     parser.add_argument('-b', dest='bootstrap', help='set bootstrap broker host', required=True)
-    parser.add_argument('--lsts', action='store_true', dest='list_topics', help='display kafka topics list',
-            required=False)
-    parser.add_argument('--lscg', action='store_true', dest='list_consumer_groups', 
+    parser.add_argument('-lts', '--list_topics', action='store_true', dest='list_topics', 
+            help='display kafka topics list', required=False)
+    parser.add_argument('-dts', '--desc_topics', dest='desc_topics', 
+            help='display topic config description (i.e. ''all'' or ''topic_1,topic2'')', required=False)
+    parser.add_argument('-lcgs','--list_consumer_groups', action='store_true', dest='list_consumer_groups', 
             help='display consumer groups list', required=False)
+    parser.add_argument('-dcg','--desc_consumer_group', dest='desc_consumer_group', 
+            help='describe consumer group', required=False)
+    parser.add_argument('-ofcg','--offset_consumer_group', action='store_true', dest='offset_consumer_group', 
+            help='consumer group offsets', required=False)
+    parser.add_argument('-g','--group', dest='consumer_group', help='consumer group name(list)', required=False),
+    parser.add_argument('-pt','--partition', dest='partition_id', help='partition id', required=False),
+#    parser.add_argument('--sintopic', dest='single_topic', help='select single topic name', required=False)
 #    parser.add_argument('--test', action='store_true', dest='test_only', 
 #            help='test for dry run (optional)', required=False)
     parser.add_argument('-c', '--create', dest='create_flag', action='store_true', 
             help='create topic (optional)', required=False)
-    parser.add_argument('-d', '--delete', dest='delete_flag', action='store_true', 
+    parser.add_argument('-d', '--delete', dest='delete_flag', action='store_true',
             help='delete topic (optional)', required=False)
+    parser.add_argument('--all', dest='all_flag', action='store_true', help='delete all flag, must used with delete_flag')
+    parser.add_argument('-t', '--topics', dest='topic_names', help='topic name(list)', required=False)
     parser.add_argument('-f', dest='file_name', help='load file (json)', required=False)
     parser.add_argument('--ssl', dest="enable_ssl", action="store_true", help='Enable SSL', required=False)
     parser.add_argument('--dry', dest="dry_flag", action="store_true", help="dry run", required=False)
@@ -47,20 +60,31 @@ def command_line():
 
 def main():
 
-    default_client_id='KPA_'+__version__
+    default_client_id='aws-msk-cli_'+__version__
     new_topic_list=[]
     default_repl_factor=3
+    count=0
 
     args = command_line()
-    print (args)
+#    print (args)
+    logger.debug("parms {}".format(args))
+#    if (args.help_flag):
+#        args.print_help()
 
     params={
             'bootstrap': args.bootstrap,
             'clientId': default_client_id,
             'listTopics': args.list_topics,
+            'descTopics': args.desc_topics,
             'listConsumerGroups': args.list_consumer_groups,
+            'descConsumerGroup': args.desc_consumer_group,
+            'offsetConsumerGroup': args.offset_consumer_group,
+            'consumerGroup': args.consumer_group,
+            'partitionId': args.partition_id,
+            'topicNames': args.topic_names,
             'createFlag': args.create_flag,
             'deleteFlag': args.delete_flag,
+            'allFlag': args.all_flag,
             'filename': args.file_name,
             'enableSSL': args.enable_ssl,
             'dryrun': args.dry_flag,
@@ -69,16 +93,82 @@ def main():
 
     if(args.debug_flag):
         logger.setLevel(logging.DEBUG)
-        logger.debug("starting debug mode ==================!")
+        logger.debug("====================starting debug mode ==================!")
 
-        logger.debug("key: {} value:{}".format(key, value))
+        for key, value in params.items():
+#            print ("key:{}, value:{}".format(key, value))
+            logger.debug("key: %s, value:%s",key, value)
             
-    K = _Admin(**params)
+    K = KafkaAdminCli(**params)
 
+    # create topic from json file
     if (args.create_flag and args.file_name):
-        K._topic_create_from_json_file()
+        if ("json" in args.file_name):
+            K._topic_create_from_json_file()
+        if ("yaml" in args.file_name) or ("yml" in args.file_name):
+            K._topic_create_from_yaml_file()
+
+    if (args.delete_flag is True):
+        if (args.topic_names is True):
+            try:
+                message = K.kafka_topic_delete()
+            except Errors as e:
+                print (e)
+        elif (args.file_name is True):
+            try:
+                message = K.kafka_topic_delete_file()
+            except Errors as e:
+                print (e)
+        elif (args.all_flag is True):
+            try:
+                message = K.kafka_topic_delete_all()
+            except Errors as e:
+                print (e)
+
+        if message is not None:
+            logger.info("topic_delete: {} completed with {}".format(args.topic_names,message))
+
+    # list of consumer group from broker
+    if (args.list_consumer_groups):
+        lcgs=K.kafka_consumer_groups()
+        lcgsc=0
+        for cg in lcgs:
+            lcgsc += 1
+            print("{}:{}".format(lcgsc,cg[0]))
+
+    if (args.desc_consumer_group):
+        logger.debug("param: {}".format(args.desc_consumer_group))
+        kdcg=K.kafka_describe_consumer_group()
+        print ("describe_consumer: {}".format(kdcg))
+
+    if (args.offset_consumer_group):
+        logger.debug("param: {} {} {}".format(args.offset_consumer_group,
+            args.consumer_group,
+            args.partition_id))
+        ocg=K.kafka_list_consumer_group_offsets()
+        print ("offset: {}".format(ocg))
+
+    # list of topics from broker
     if (args.list_topics):
-        K.kafka_consumer_groups()
+       lt = K.kafka_list_topics()
+       lt.sort()
+       ltc=0
+       logger.info("print type of List_topic: %s", type(lt))
+       for tp in lt:
+           ltc += 1
+           logger.info('topics[{}]: {}'.format(ltc,tp))
+
+    # descibe topics, output json foramt
+    if (args.desc_topics == 'all') or (args.desc_topics is not None):
+        dt = K.kafka_describe_topics()
+        #dt.sort()
+        logger.debug("type of desc topics: %s", type(dt))
+        logger.debug("topics: {}".format(dt))
+        dtc=0
+        for tp in dt:
+            #logger.info('detailed: {} \n'.format(tp))
+            dtc += 1
+            print ("describe-Topics[{}]: {} \n".format(dtc,json.dumps(tp, indent=4)))
 
 if __name__ == "__main__":
     main()
--- KafkaCLI/client.py
+++ KafkaCLI/client.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
 
-import os
+#import os
 import sys
 import json
 import logging
+import yaml
 # from confluent_kafka.admin import AdminClient, NewTopic
-from kafka.admin import NewTopic
-from kafka.admin import KafkaAdminClient
-from kafka.errors import TopicAlreadyExistsError
+from kafka.client_async import KafkaClient
+from kafka.admin import NewTopic, KafkaAdminClient
+from kafka.errors import TopicAlreadyExistsError, NoBrokersAvailable, UnknownTopicOrPartitionError
 
-#logger = logging.getLogger(__name__)
-#logger.setLevel(logging.DEBUG)
-#out_hdlr = logging.StreamHandler(sys.stdout)
-#logger.addHandler(out_hdlr)
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# out_hdlr = logging.StreamHandler(sys.stdout)
+# logger.addHandler(out_hdlr)
+
+class Error(Exception):
+    pass
+
+class TopicDeletionError(Error):
+    pass
 
 class Config(object):
+    '''
+        Config Class
+        mapping external kwargs to internal properties
+    '''
     def __init__(self, **kwargs):
+
         for key, value in kwargs.items():
             setattr(self, key, value)
     @property
@@ -29,7 +41,7 @@ class Config(object):
     def _selectrTopic(self):
         return getattr(self,"selectTopic")
     @property
-    def _topicList(self):
+    def _listTopics(self):
         return getattr(self,"listTopics")
     @property
     def _ssl(self):
@@ -37,65 +49,185 @@ class Config(object):
     @property
     def _file(self):
         return getattr(self,"filename")
+    @property
     def _testOnly(self):
         return getattr(self,"dryrun")
+    @property
+    def _createTopics(self):
+        return getattr(self,"createFlag")
+    @property
+    def _deleteTopics(self):
+        return getattr(self,"deleteFlag")
+    @property
+    def _topicNames(self):
+        return getattr(self,"topicNames")
+    @property
+    def _listConsumerGroups(self):
+        return getattr(self,"listConsumerGroups")
+    @property
+    def _listTopics(self):
+        return getattr(self,"listTopics")
+    @property
+    def _descTopics(self):
+        return getattr(self,"descTopics")
+    @property
+    def _descConsumerGroup(self):
+        return getattr(self,"descConsumerGroup")
+    @property
+    def _offsetConsumerGroup(self):
+        return getattr(self,"offsetConsumerGroup")
+    @property
+    def _consumerGroup(self):
+        return getattr(self,"consumerGroup")
+    @property
+    def _partitionId(self):
+        return getattr(self,"partitionId")
+#    def get_property(self, property_name):
+#        if property_name not in self.kwargs.keys():
+#            return None
+#        return self.kwargs[property_name]
 
-class _Admin(Config):
+class KafkaAdminCli(Config):
     '''
+        HMKL Kafkacli.
         create topic
         delete topic
         rebalance topics
     '''
 
-#    def __init__(self):
-#        self.connection_string = default_bootstrap
-#        self.client_id = default_client_id
-#        self.in_file = default_file
-
-
     def __init__(self, **kwargs):
-        super(_Admin, self).__init__(**kwargs)
+        super(KafkaAdminCli, self).__init__(**kwargs)
         self._topic_list=[]
         self._connection=None
+        self._topic_name=None
+        self._topic_partition=None
+        self._topic_replica=None
+        self._topic_assignments=None
+        self._topic_configs=None
 
-        print(self.__dir__())
+#        print(self.__dir__())
 
         if self._connection is None:
-            if self._ssl is None:
+            if (self._ssl is None) or (self._ssl is False):
                 self._connection=self.kafka_admin_connect(secure='PLAINTEXT')
             else:
                 self._connection=self.kafka_admin_connect(secure='SSL')
 
-        if self.dryrun is None:
-            self._testOnly = false
+#        print ("kwargs: {}\n, _testOnly: {} ".format(kwargs,self._testOnly.__getattribute__))
+#        logger.debug( "dir: %s", kwargs.__dir__)
+#        if self.dryrun is None:
+#            self._testOnly = false
+#        print("self._testOnly: {} ".format(self._testOnly))
 
     def kafka_admin_connect(self, secure=None):
         '''
-        connect kafka 
+        connect kafka
         '''
-        self._connection = KafkaAdminClient(bootstrap_servers=self.bootstrap, client_id=self.clientId, security_protocol=secure)
+        logger.info("connecting via KafkaAdminClient")
+        try:
+            self._connection = KafkaAdminClient(
+                    bootstrap_servers=self.bootstrap, 
+                    client_id=self.clientId,
+                    security_protocol=secure)
+        except NoBrokersAvailable as e:
+            print ("kafka_admin_connect: {} NoBrokerAailable Error!".format(e))
+        logger.debug("client connected: %s",self._connection)
         return self._connection
 
     def _topic_create_from_json_file(self):
         '''
         create topic from topic.json file
+        return self.kafka_topic_create
         '''
-        jsonData = self._open_file(self._file)
+        jsonData = self._open_file(self._file, "json")
+#        self.dup_check(jsonData)
+#        logging.debug('{}{}'.format(self._listTopics, self._file))
 
-        for j in jsonData['topic']:
-            tn = j['name']
-            tp = j['partition']
-            tr = j['replication']
-            bundle = self.topic_binding(tn, tp, tr)
-            logging.debug('{}{}{}'.format(tn, tp, tr))
-            self._topic_list.append(bundle)
-        return self.kafka_topic_create(self._topic_list, self._testOnly)
+        list_topic= []
+        for item in jsonData['topic']:
+            print (item)
+            ret = self._topic_formater_binder(item)
+            list_topic.append(ret)
+        print("list_topic: {}".format(list_topic))
+        #size_of_dict = len(jsonData['topic'][0].keys())
 
+        return self.kafka_topic_create(list_topic, self._testOnly)
+
+
+    def _topic_create_from_yaml_file(self):
+        '''
+        create topic from topic.json file
+        return self.kafka_topic_create
+        '''
+        jsonData = self._open_file(self._file, "yaml")
+#        self.dup_check(jsonData)
+#        logging.debug('{}{}'.format(self._listTopics, self._file))
+
+        list_topic= []
+        for item in jsonData['topic']:
+            print (item)
+            ret = self._topic_formater_binder(item)
+            list_topic.append(ret)
+        print("list_topic: {}".format(list_topic))
+        #size_of_dict = len(jsonData['topic'][0].keys())
+
+#        return self.kafka_topic_create(list_topic, self._testOnly)
+
+
+
+    def _topic_formater_binder(self, Data):
+        '''
+        :toppic_formater_binder:
+        topics with mixed formatted topic config to correct format
+        return set_binder
+        '''
+        _topic_format = {
+                "name": None,
+                "partition": None,
+                "replica": None,
+                "assignments": None,
+                "configs": None
+                }
+
+        for key, value in Data.items():
+            #print ("K:{}, V:{}".format(key, value))
+            if key == 'name':
+                self._topic_name = value
+            elif key == 'partition':
+                self._topic_partition = value
+            elif key == 'replica':
+                self._topic_replica = value
+            elif (key == 'assignments' and value != 'default'):
+                self._topic_assignments = value[0]
+            elif (key == 'configs'):
+                self._topic_configs = value[0]
+
+        _topic_format['name']=self._topic_name
+        _topic_format['partition']=self._topic_partition
+        _topic_format['replica']=self._topic_replica
+        _topic_format['assignments']=self._topic_assignments
+        _topic_format['configs']=self._topic_configs
+
+        logger.debug ("topic_name: %s",self._topic_name)
+        logger.debug ("topic_partition: %s",self._topic_partition)
+        logger.debug ("topic_replica: %s",self._topic_replica)
+        logger.debug ("topic_replica_assignment: [%s]",self._topic_assignments)
+        logger.debug ("topic_configuration: [%s]",self._topic_configs)
+
+        return self._newtopic_binding(
+                _topic_format['name'],
+                _topic_format['partition'],
+                _topic_format['replica'],
+                _topic_format['assignments'],
+                _topic_format['configs'])
 
     def kafka_topic_create(self, topic_list, flag=None):
         '''
         create_topics
         '''
+        if flag is not None:
+            flag=self._testOnly
+#        logger.debug("flag: %s, _testOnly: %s", flag.__self__, self._testOnly.__self__)
         try:
             self._connection.create_topics(new_topics=topic_list,
                                                     validate_only=flag)
@@ -103,80 +235,157 @@ class _Admin(Config):
             print("Type: ", type(t), "TopicAlreadyExistsError:: \n", t)
             pass
 
-    def kafka_topic_delete(self, topic_list):
+    def kafka_topic_delete(self):
         '''
-        delete_topics
+        kafka_topic_delete
+        '''
+        logger.debug("_deleteTopic:{}, _testOnly:{}, _topicNames:{}".format(self._deleteTopics, self._testOnly, self._topicNames))
+        if (self._deleteTopics):
+            return self._topic_delete(topic_list=self._topicNames, flag=self._testOnly)
+        else: 
+            raise ("delete topic flag has not been triggerred")
+
+    def _topic_delete(self, topic_list=None, flag=None):
+        '''
+        _topic_delete
         parms: topic_list (string)
         '''
+        #topic_list=[]
+        logger.debug("topic_list:{} is going to be deleted!".format(topic_list))
+        if topic_list is None or len(topic_list)==0:
+            raise TopicDeletionError("at least more than one topic need to be input")
+        else:
+            new_list=topic_list.split(",")
+            logger.info("newlist: %s",new_list)
+
+        if flag is not None:
+            flag=self._testOnly
         try:
-            self._connection.delete_topics(topic_list)
+            self._connection.delete_topics(new_list)
         except UnknownTopicOrPartitionError as u:
+            logger.debug("Error while deleteing topic: {}".format(type(u)))
             print("TypeL: ", type(u), "UnknownTopicOrPartitionError:: \n", u)
-            pass
+
+    def kafka_list_topics(self):
+        logger.info("Listing Topics")
+        return self._connection.list_topics()
+
+    def kafka_describe_topics(self):
+
+        _topic=[]
+        
+        if (self._descTopics == 'all') or len(self._descTopics) == 0:
+            _topic=None
+        else:
+            logger.debug("type: {}, list: {}".format(type(self._descTopics), self._descTopics))
+            _topic=self._descTopics.split(',')
+        
+
+        logger.info("Describe Topics")
+        return self._connection.describe_topics(_topic)
+          
 
     def kafka_consumer_groups(self, Broker_ids=None):
-            result2 = self._connection.list_consumer_groups(Broker_ids)
-            print ("Listing Consumer Groups")
-            print ("result: {}".format(result2))
-
-    def dup_checker(self, JasonData):
-        _duplist = []
-        for k in JasonData["topic"].keys():
-            if dict[k]==0:
-                dict[k]=1
-            elif dict[k]>0:
-                dict[k]+=1
-                _dup.append(k) 
-        return _duplist
+        #result2 = self._connection.list_consumer_groups(Broker_ids)
+        logger.info("Listing Consumer Groups")
+        return self._connection.list_consumer_groups(Broker_ids)
 
     def _load_Json(self, data):
         '''
         load json data to directionary
         param : json data
-        return: json dictionary 
+        return: json dictionary
         '''
         return json.load(data)
 
-    def _open_file(self, filename):
+    def _open_file(self, filename, fileType):
         '''
         open file curor to read context
         return: file
         '''
         with open(filename, 'rt') as rtfile:
-            outfile = self._load_Json(rtfile)
+        #   print (type(file))
+            if (fileType == "json"):
+                outfile = self._load_Json(rtfile)
+            elif (fileType == "yaml"):
+                outyaml=yaml.safe_load(rtfile)
+                logger.debug("yaml: %s",outyaml)
+                # print (type(outyaml))
+                outfile = outyaml
+        logger.debug("output (dictionary): %s",outfile)
         return outfile
 
-    def topic_binding(self, topic_name, topic_parition, topic_replica):
+    def _newtopic_binding(self, topic_name, topic_parition, topic_replica, topic_replica_assign=None, topic_configs=None):
         '''
-        binding topic criteria of NewTopic class
+        binding newtopic with criteria of NewTopic class
 
         required: name(string), num_partitions(int), replication_factor(int)
-        optional: replication_assignment(dict(int)), topic_configs(dict(str))
+        optional: replica_assignment(dict(int)), topic_configs(dict(str))
 
         return: instance of class
         '''
         NT = NewTopic(name=topic_name,
                       num_partitions=topic_parition,
-                      replication_factor=topic_replica)
+                      replication_factor=topic_replica,
+                      replica_assignments=topic_replica_assign,
+                      topic_configs=topic_configs)
+
         return NT
 
 
-'''
+    def kafka_api_version(self):
+        '''
+        get api version
+        '''
+        return self._connection.get_api_versions()
+
+
+    def kafka_describe_consumer_group(self):
+        '''
+        describe consumer group
+        :params: consumer_group_id (string)
+        '''
+        _consumer_group_id=[]
+
+        if self._descConsumerGroup is None or len(self._descConsumerGroup) == 0:
+            _consumer_group_id=None
+        else:
+            _consumer_group_id=self._descConsumerGroup.split(",")
+
+        return self._connection.describe_consumer_groups(group_ids=_consumer_group_id)
+
+
+    def kafka_list_consumer_group_offsets(self):
+        '''
+        list consumer group offsets
+        '''
+        if self._partitionId is None:
+            partition_id=None
+        else:
+            partition_id=self._partitionId.split(",")
+        
+        if self._consumerGroup is None or self._consumerGroup == 'all' or self._consumerGroup == 'ALL':
+            consumer_group_id=self.kafka_consumer_groups()
+        else:
+            consumer_group_id=self._consumerGroup.split(",")
+
+        return self._connection.list_consumer_group_offsets(group_id=consumer_group_id, partitions=partition_id)
+
+
 def main():
     topic_list = []
 
-    file = "./kafka-topics.json"
+    file = "../topics/usps.events.v1.json"
     jsonData = open_file(file)
     for j in jsonData['topic']:
         tn = j['name']
         tp = j['partition']
-        tr = 1
-        bundle = topic_binding(tn, tp, tr)
+        tr = j['replica']
+        cf = j['configs'][0]
+        bundle = newtopic_binding(tn, tp, tr, cf)
         topic_list.append(bundle)
-
 
     print ("{}".format(topic_list))
 
 if __name__ == "__main__":
     main()
-'''
